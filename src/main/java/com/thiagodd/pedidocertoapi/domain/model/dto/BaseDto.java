2f36f2b665877261008f3411af8a54939bba991e
Thiagodd <Thiagodd.dev@gmail.com>
2023-10-31 11:51:20 -0300

    feat: add attribute id, equals and hashCode in BaseDto

--- src/main/java/com/thiagodd/pedidocertoapi/domain/model/dto/BaseDto.java
+++ src/main/java/com/thiagodd/pedidocertoapi/domain/model/dto/BaseDto.java
@@ -2,7 +2,25 @@ package com.thiagodd.pedidocertoapi.domain.model.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+import java.util.Objects;
 import java.util.UUID;
 
-public abstract class BaseDto {
+@Data
+public abstract class BaseDto implements Serializable {
+
+    UUID id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseDto baseDto = (BaseDto) o;
+        return Objects.equals(id, baseDto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
